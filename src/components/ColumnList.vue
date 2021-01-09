ab9595f01b662fe17d6e3b687fde513be2e7e968
yang <1308380266@qq.com>
2021-01-09 17:29:17 +0800

    ColumnList组件创建

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,29 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import {defineComponent,PropType} from "vue"
+export interface ColumProps{
+    id:number;
+    title:string;
+    avatar:string;
+    description:string;
+}
+export default defineComponent({
+    name:'ColumnList',
+    props:{
+        list:{
+            type:Array as PropType<ColumProps[]>,
+            required:true
+        }
+    },
+    setup(props,context){
+        
+    }
+})
+</script>
+
+<style>
+
+</style>
